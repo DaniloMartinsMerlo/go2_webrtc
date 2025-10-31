664ab925842dc23d01b45b3eced9bb3f6ca59652
yasmimpassos <yasmim.passos@sou.inteli.edu.br>
2025-10-31 12:56:11 -0300

    feat: inicializa movimentacoes

--- testes/move.py
+++ testes/move.py
@@ -24,18 +24,18 @@ async def main():
             current_motion_switcher_mode = data['name']
             print(f"Current motion mode: {current_motion_switcher_mode}")
 
-            if current_motion_switcher_mode != "normal":
-                print(f"Switching motion mode from {current_motion_switcher_mode} to 'normal'...")
-                await conn.datachannel.pub_sub.publish_request_new(
-                    RTC_TOPIC["MOTION_SWITCHER"], 
-                    {
-                        "api_id": 1002,
-                        "parameter": {"name": "normal"}
-                    }
-                )
-                await asyncio.sleep(5)
+        if current_motion_switcher_mode != "normal":
+            print(f"Switching motion mode from {current_motion_switcher_mode} to 'normal'...")
+            await conn.datachannel.pub_sub.publish_request_new(
+                RTC_TOPIC["MOTION_SWITCHER"], 
+                {
+                    "api_id": 1002,
+                    "parameter": {"name": "normal"}
+                }
+            )
+            await asyncio.sleep(10)
 
-        movement_duration = 3
+        movement_duration = 4
         start_time = time.time()
         end_time = start_time + movement_duration
 
@@ -49,7 +49,7 @@ async def main():
                     "parameter": {"x": 1, "y": 0, "z": 0}
                 }
             )
-            await asyncio.sleep(0.9)
+            await asyncio.sleep(0.95)
 
         print("Movement time finished. Stopping robot...")
 
@@ -61,6 +61,61 @@ async def main():
         )
 
         await asyncio.sleep(1)
+
+        movement_duration = 1.7
+        start_time = time.time()
+        end_time = start_time + movement_duration
+
+        print(f"Starting rotation movement for {movement_duration} seconds")
+
+        while time.time() <= end_time:
+            await conn.datachannel.pub_sub.publish_request_new(
+                RTC_TOPIC["SPORT_MOD"], 
+                {
+                    "api_id": SPORT_CMD["Move"],
+                    "parameter": {"x": 0, "y": 0, "z": 1}
+                }
+            )
+            await asyncio.sleep(1)
+
+        print("Movement time finished. Stopping robot...")
+
+        await conn.datachannel.pub_sub.publish_request_new(
+            RTC_TOPIC["SPORT_MOD"], 
+            {
+                "api_id": SPORT_CMD["StopMove"]
+            }
+        )
+
+        await asyncio.sleep(1)
+
+
+        movement_duration = 4
+        start_time = time.time()
+        end_time = start_time + movement_duration
+
+        print(f"Starting forward movement for {movement_duration} seconds")
+
+        while time.time() <= end_time:
+            await conn.datachannel.pub_sub.publish_request_new(
+                RTC_TOPIC["SPORT_MOD"], 
+                {
+                    "api_id": SPORT_CMD["Move"],
+                    "parameter": {"x": 1, "y": 0, "z": 0}
+                }
+            )
+            await asyncio.sleep(0.95)
+
+        print("Movement time finished. Stopping robot...")
+
+        await conn.datachannel.pub_sub.publish_request_new(
+            RTC_TOPIC["SPORT_MOD"], 
+            {
+                "api_id": SPORT_CMD["StopMove"]
+            }
+        )
+
+        await asyncio.sleep(2)
 
         print("Done.")
 
